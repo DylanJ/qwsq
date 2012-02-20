3ce072b33ba99c71c90d373d3d0510584ca97aaf
Dylan Johnston <qdylanj@gmail.com>
2012-02-19 21:37:07 -0800

    put into classes

--- qw.rb
+++ qw.rb
@@ -12,28 +12,52 @@ def quake_text str
   str.bytes.to_a.collect{ |c| @quake_charset[c] }.reject{ |c| c == 0xFF.chr }.join
 end 
 
-# <3
-class ServerAddress < BitStruct
-  unsigned :ip_0,   8,  "First Octet"
-  unsigned :ip_1,   8,  "Second Octet"
-  unsigned :ip_2,   8,  "Third Octet"
-  unsigned :ip_3,   8,  "Fourth Octet"
-  unsigned :port,  32,  "Port", :endian => :big
+module QW 
+  
+  class Util
+    def self.send_cmd( ip, port, proto )
+      cmd_to_send = "#{proto}\n\0"
+
+      sock = UDPSocket.new
+      sock.connect( ip, port )
+      sock.send cmd_to_send, 0
+      sock.recvfrom(204800) # i saw a number around this size once.
+    end
+  end
+  
+  class ServerAddress < BitStruct
+    unsigned :ip_0,   8,  "First Octet"
+    unsigned :ip_1,   8,  "Second Octet"
+    unsigned :ip_2,   8,  "Third Octet"
+    unsigned :ip_3,   8,  "Fourth Octet"
+    unsigned :port,  32,  "Port", :endian => :big
+  end
+
+  class Server
+    attr_reader :rules
+      
+    
+
+  end
+
+  class Master
+    attr_reader :hostname, :map
+  end
+
 end
+
+ap x = QW::Server.new 
+ap QW::Server.get_poop
+
+__END__
+
+# <3
 
 QWM_SERVERLIST = 'c'
 QWS_SERVERINFO = "\xFF\xFF\xFF\xFFstatus" 
 @player_regex = /(?<id>\d+) (?<frags>-?\d+) (?<time>\d+) (?<ping>\d+) \"(?<name>.*)\" \"(?<skin>.*)\" (?<shirt>\d+) (?<pants>\d+)/
 @quake_charset = "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF[]0123456789\xFF\xFF\xFF\xFF !\"\#$%&'()*+,-./0123456789:;(=)?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_'abcdefghijklmnopqrstuvwxyz{|}~<\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF[]0123456789\xFF\xFF\xFF\xFF !\"\#$%&'()*+,-./0123456789:;(=)?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_'abcdefghijklmnopqrstuvwxyz{|}~<"
 
-def send_cmd( ip, port, proto )
-  cmd_to_send = "#{proto}\n\0"
-
-  sock_master = UDPSocket.new
-  sock_master.connect( ip, port )
-  sock_master.send cmd_to_send, 0
-  sock_master.recvfrom(204800) # i saw a number around this size once.
-end
 
 def get_server_info( ip, port )
   data = send_cmd( ip, port, QWS_SERVERINFO )[0]
@@ -51,7 +75,7 @@ def get_server_info( ip, port )
 end
 
 def get_servers_from_master( ip, port )
-  data = send_cmd( ip, port, 27000, QWM_SERVERLIST )
+  data = send_cmd( ip, port, QWM_SERVERLIST )
 
   num_addr_returned = data[0].length / 6
   servers = []
@@ -66,6 +90,14 @@ def get_servers_from_master( ip, port )
 end
 
 #ap get_server_info( "74.86.171.201", 27500 )
-ap get_server_info( "74.86.171.201", 27500 )
-#ap get_servers_from_master( "188.40.112.251", 27000 )
+#ap get_server_info( "74.86.171.201", 27500 )
+servers = []
+get_servers_from_master( "188.40.112.251", 27000 ).each do |server|
+  if ( server[:ip] == "255.255.255.255" )
+    next
+  end
+  servers << get_server_info( server[:ip], server[:port] )
+end
+
+ap servers
 
